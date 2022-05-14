946f8f27ba3d2d1ffe4aeb06acffbd22ed2d38d5
ruhulamin111 <ruhuljuwel111@gmail.com>
2022-05-15 00:41:11 +0600

    add socail sing in

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,9 @@
+import { async } from '@firebase/util';
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import SocialSignIn from './SocialSignIn';
 
 const SignUp = () => {
@@ -10,12 +13,20 @@ const SignUp = () => {
 
     const navigate = useNavigate();
 
-    const handleSignUp = event => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleSignUp = async (event) => {
         event.preventDefault();
         const name = nameRef.current.value;
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(name, email, password);
+        await createUserWithEmailAndPassword(email, password)
+        navigate('/home')
     }
 
     const navigateSignIn = event => {
--- src/components/pages/SocialSignIn.js
+++ src/components/pages/SocialSignIn.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const SocialSignIn = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
+
         <div>
             <h3>this is socail login </h3>
+            <div>
+                <button onClick={() => signInWithGoogle()} className=''>Google Sign In</button>
+
+            </div>
         </div>
     );
 };
